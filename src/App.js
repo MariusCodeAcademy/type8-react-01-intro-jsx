e7513d08423a2691ae0cd9bc5f8806057df2dc91
Marius <marius@example.com>
2022-05-30 11:57:54 +0300

    css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,16 @@ import './app.css';
 import img1 from './newyork.jpg';
 const username = 'James';
 
-const subTitle = <h2>I am sub title</h2>;
+const card = (
+  <article className='card'>
+    <img className='card-image' src={img1} alt='Card' />
+    <div className='card-content'>
+      <h3>paris</h3>
+      <p className='date'>{new Date().toDateString()}</p>
+      <p className='text'>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+    </div>
+  </article>
+);
 
 function add(n1, n2) {
   return n1 + n2;
